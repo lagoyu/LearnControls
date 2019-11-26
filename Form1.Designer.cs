a01046281ff45800b62f3d2eaf2b145a41a88943
Paul Shaddick <Paul.Shaddick@berkeleygreenutc.org.uk>
2019-11-26 10:27:12 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace LearnControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnWelcome = new System.Windows.Forms.Button();
+            this.grpColour = new System.Windows.Forms.GroupBox();
+            this.radRed = new System.Windows.Forms.RadioButton();
+            this.radGreen = new System.Windows.Forms.RadioButton();
+            this.radBlue = new System.Windows.Forms.RadioButton();
+            this.radWhite = new System.Windows.Forms.RadioButton();
+            this.chkShow = new System.Windows.Forms.CheckBox();
+            this.lblClock = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tmrClock = new System.Windows.Forms.Timer(this.components);
+            this.grpColour.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(63, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Enter Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(81, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(125, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnWelcome
+            // 
+            this.btnWelcome.Location = new System.Drawing.Point(212, 2);
+            this.btnWelcome.Name = "btnWelcome";
+            this.btnWelcome.Size = new System.Drawing.Size(75, 23);
+            this.btnWelcome.TabIndex = 2;
+            this.btnWelcome.Text = "Welcome";
+            this.btnWelcome.UseVisualStyleBackColor = true;
+            this.btnWelcome.Click += new System.EventHandler(this.btnWelcome_Click);
+            // 
+            // grpColour
+            // 
+            this.grpColour.Controls.Add(this.radWhite);
+            this.grpColour.Controls.Add(this.radBlue);
+            this.grpColour.Controls.Add(this.radGreen);
+            this.grpColour.Controls.Add(this.radRed);
+            this.grpColour.Location = new System.Drawing.Point(12, 31);
+            this.grpColour.Name = "grpColour";
+            this.grpColour.Size = new System.Drawing.Size(164, 119);
+            this.grpColour.TabIndex = 3;
+            this.grpColour.TabStop = false;
+            this.grpColour.Text = "Choose your favourite colour";
+            // 
+            // radRed
+            // 
+            this.radRed.AutoSize = true;
+            this.radRed.Location = new System.Drawing.Point(7, 20);
+            this.radRed.Name = "radRed";
+            this.radRed.Size = new System.Drawing.Size(45, 17);
+            this.radRed.TabIndex = 0;
+            this.radRed.TabStop = true;
+            this.radRed.Text = "Red";
+            this.radRed.UseVisualStyleBackColor = true;
+            this.radRed.CheckedChanged += new System.EventHandler(this.radRed_CheckedChanged);
+            // 
+            // radGreen
+            // 
+            this.radGreen.AutoSize = true;
+            this.radGreen.Location = new System.Drawing.Point(7, 44);
+            this.radGreen.Name = "radGreen";
+            this.radGreen.Size = new System.Drawing.Size(54, 17);
+            this.radGreen.TabIndex = 1;
+            this.radGreen.TabStop = true;
+            this.radGreen.Text = "Green";
+            this.radGreen.UseVisualStyleBackColor = true;
+            this.radGreen.CheckedChanged += new System.EventHandler(this.radGreen_CheckedChanged);
+            // 
+            // radBlue
+            // 
+            this.radBlue.AutoSize = true;
+            this.radBlue.Location = new System.Drawing.Point(7, 68);
+            this.radBlue.Name = "radBlue";
+            this.radBlue.Size = new System.Drawing.Size(46, 17);
+            this.radBlue.TabIndex = 2;
+            this.radBlue.TabStop = true;
+            this.radBlue.Text = "Blue";
+            this.radBlue.UseVisualStyleBackColor = true;
+            this.radBlue.CheckedChanged += new System.EventHandler(this.radBlue_CheckedChanged);
+            // 
+            // radWhite
+            // 
+            this.radWhite.AutoSize = true;
+            this.radWhite.Location = new System.Drawing.Point(7, 92);
+            this.radWhite.Name = "radWhite";
+            this.radWhite.Size = new System.Drawing.Size(53, 17);
+            this.radWhite.TabIndex = 3;
+            this.radWhite.TabStop = true;
+            this.radWhite.Text = "White";
+            this.radWhite.UseVisualStyleBackColor = true;
+            this.radWhite.CheckedChanged += new System.EventHandler(this.radWhite_CheckedChanged);
+            // 
+            // chkShow
+            // 
+            this.chkShow.AutoSize = true;
+            this.chkShow.Checked = true;
+            this.chkShow.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkShow.Location = new System.Drawing.Point(182, 40);
+            this.chkShow.Name = "chkShow";
+            this.chkShow.Size = new System.Drawing.Size(105, 17);
+            this.chkShow.TabIndex = 4;
+            this.chkShow.Text = "Show colour box";
+            this.chkShow.UseVisualStyleBackColor = true;
+            this.chkShow.CheckedChanged += new System.EventHandler(this.chkShow_CheckedChanged);
+            // 
+            // lblClock
+            // 
+            this.lblClock.AutoSize = true;
+            this.lblClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClock.Location = new System.Drawing.Point(182, 64);
+            this.lblClock.Name = "lblClock";
+            this.lblClock.Size = new System.Drawing.Size(88, 24);
+            this.lblClock.TabIndex = 5;
+            this.lblClock.Text = "00:00:00";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(190, 91);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(190, 117);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tmrClock
+            // 
+            this.tmrClock.Interval = 1000;
+            this.tmrClock.Tick += new System.EventHandler(this.tmrClock_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 162);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblClock);
+            this.Controls.Add(this.chkShow);
+            this.Controls.Add(this.grpColour);
+            this.Controls.Add(this.btnWelcome);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpColour.ResumeLayout(false);
+            this.grpColour.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnWelcome;
+        private System.Windows.Forms.GroupBox grpColour;
+        private System.Windows.Forms.RadioButton radWhite;
+        private System.Windows.Forms.RadioButton radBlue;
+        private System.Windows.Forms.RadioButton radGreen;
+        private System.Windows.Forms.RadioButton radRed;
+        private System.Windows.Forms.CheckBox chkShow;
+        private System.Windows.Forms.Label lblClock;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer tmrClock;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearnControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWelcome_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome " + txtName.Text);
+        }
+
+        private void radRed_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void radGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void radBlue_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void radWhite_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
+
+        private void chkShow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkShow.Checked)
+            {
+                grpColour.Visible = true;
+            }
+            else
+            {
+                grpColour.Visible = false;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrClock.Enabled = true;
+        }
+        
+        private void tmrClock_Tick(object sender, EventArgs e)
+        {
+            lblClock.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmrClock.Enabled = false;
+        }
+    }
+}
